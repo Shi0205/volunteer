23defd63506437568831e80a33886f719611d9f2
Shi0205 <113345322+Shi0205@users.noreply.github.com>
2022-10-01 17:13:19 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php 
 
-$servername = "http://app-skiles.g669jaiwfs-xmz4qmwjw62o.p.temp-site.link/";
-$username = "lim0502";
-$password = "lim0502";
-$dbname = "volunteerData";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
  ?>
