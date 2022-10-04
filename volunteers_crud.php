51e683a568cc751c029943459c0ca12ea494347f
Shi0205 <113345322+Shi0205@users.noreply.github.com>
2022-10-04 22:16:44 +0800

    Update volunteers_crud.php

--- volunteers_crud.php
+++ volunteers_crud.php
@@ -48,6 +48,50 @@ if (isset($_SESSION['login'])) {
             echo "Error: " . $e->getMessage();
         }
     } // NOTE: End create
+    
+      // Update
+    if (isset($_POST['update'])) {
+        if ($_SESSION['admin'] == true) {
+            try {
+                $conn->beginTransaction();
+
+                $stmt1 = $conn->prepare("UPDATE tbl_volunteers SET fld_volunteer_fname=:fname, fld_volunteer_lname=:lname, fld_volunteer_email=:email, fld_volunteer_gender=:gender, fld_volunteer_phone=:phone, fld_volunteer_address=:address  WHERE fld_volunteer_id=:vid");
+
+                $stmt1->bindParam(':vid', $vid, PDO::PARAM_STR);
+                $stmt1->bindParam(':fname', $fname, PDO::PARAM_STR);
+                $stmt1->bindParam(':lname', $lname, PDO::PARAM_STR);
+                $stmt1->bindParam(':email', $email, PDO::PARAM_STR);
+                $stmt1->bindParam(':gender', $gender, PDO::PARAM_STR);
+                $stmt1->bindParam(':phone', $phone, PDO::PARAM_STR);
+                $stmt1->bindParam(':address', $address, PDO::PARAM_STR);
+
+                $vid = $_POST['oldid'];
+                $fname = $_POST['fname'];
+                $lname = $_POST['lname'];
+                $email = $_POST['email'];
+                $pswd = $_POST['passwordd'];
+                $gender = $_POST['gender'];
+                $phone = $_POST['phone'];
+                $address = $_POST['address'];
+
+                $stmt1->execute();
+
+                $stmt2 = $conn->prepare("UPDATE tbl_users SET fld_user_email=:email, fld_user_password =:pswd WHERE fld_user_id=:vid");
+
+                $stmt2->bindParam(":vid", $vid, PDO::PARAM_STR);
+                $stmt2->bindParam(":email", $email, PDO::PARAM_STR);
+                $stmt2->bindParam(":pswd", $pswd, PDO::PARAM_STR);
+                $stmt2->execute();
+                $conn->commit();
+                
+                
+            } catch (PDOException $e) {
+                $conn->rollback();
+                echo "Error: " . $e->getMessage();
+            }
+        }
+    } // End of Update
+
 
     // Delete
     if (isset($_GET['delete'])) {
