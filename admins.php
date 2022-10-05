81db7e96b8be913435f6deacc6c42627c0f5fc2b
Shi0205 <113345322+Shi0205@users.noreply.github.com>
2022-10-05 12:53:48 +0800

    Update admins.php

--- admins.php
+++ admins.php
@@ -59,7 +59,7 @@ if (isset($_SESSION['admin'])) {
     <?php include_once 'nav_bar.php'; ?>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-xs-12 col-sm-9 col-sm-offset-1 col-md-7 col-md-offset-2">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 
                 <div class="page-header">
                     <h2>Add New Admin</h2>
