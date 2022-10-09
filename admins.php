07156b8da9b0d093bf9c95ce64b1fadce6f1fd69
Shi0205 <113345322+Shi0205@users.noreply.github.com>
2022-10-09 23:22:51 +0800

    Update admins.php

--- admins.php
+++ admins.php
@@ -52,10 +52,29 @@ if (isset($_SESSION['admin'])) {
     </script>
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
+    
+    
+ <style>
+    .bg-5 {
+      /*background-color: #2f2f2f;*/
+      background-color: #4f9a94;
+      color: #ffffff;
+      margin-top: 30px;
+      padding-top: 50px;
+      padding-bottom: 50px;
+      padding-left: 50px;
+    }
+
+    footer.glyphicon {
+      font-size: 20px;
+      margin-bottom: 20px;
+      color: #ffffff;
+    } 
+ </style>
 
 </head>
 
-<body>
+<body id="myPage">
     <?php include_once 'nav_bar.php'; ?>
     <div class="container-fluid">
         <div class="row">
@@ -209,11 +228,15 @@ if (isset($_SESSION['admin'])) {
             });
         });
     </script>
-</body>
     
-<footer>
-    <br>
-    <br>
+ <footer class="container-fluid bg-5 text-center">
+    <a href="#myPage" title="To Top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </a>
+    <p>Copyright @ Star Tech's Website 2022 </p>
 </footer>
+
+</body>
+
 
 </html>
